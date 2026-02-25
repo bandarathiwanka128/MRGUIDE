d7cd5316e463ed491d8a9fbabfa48fd81b8a26b9
bandarathiwanka128 <bandarathiwanka128@gmail.com>
2026-02-25 20:00:23 +0530

    Route optimizer dev

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+
 import { API_BASE_URL } from '../config';
 import './HomePage.css';
 
